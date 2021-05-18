e12106f021f8a162b13ba0a3de328975be90a5c7
berit-stange <76659950+berit-stange@users.noreply.github.com>
2021-05-18 19:00:26 +0200

    4.3 added first test for 'render list of events'
    using shallow rendering

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import App from '../App';
+import App from '../App'; // from App.js > can’t test component without importing this
 
-describe('<App /> component', () => {
-
+// create group/scope via describe function
+describe('<App /> component', () => { // test description: accurate enough to be found in long list of tests
+    test('render list of events', () => {
+        const AppWrapper = shallow(<App />); // use shallow rendering API, App is parameter, set it to constant AppWrapper
+        expect(AppWrapper.find(EventList)).toHaveLength(1); // expect() runs a search with find(), searching for EventList components within AppWrapper > only 1 should exist
+    });
 });
