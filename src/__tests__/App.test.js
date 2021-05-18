04fa670fb103c58e1b9cb9a27fcf241d312c3c1e
berit-stange <76659950+berit-stange@users.noreply.github.com>
2021-05-18 19:34:36 +0200

    4.3 deleted comments in app.test.js

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import App from '../App'; // from App.js > can’t test component without importing this
+import App from '../App';
 
-// create group/scope via describe function
-describe('<App /> component', () => { // test description: accurate enough to be found in long list of tests
+
+describe('<App /> component', () => {
     test('render list of events', () => {
-        const AppWrapper = shallow(<App />); // use shallow rendering API, App is parameter, set it to constant AppWrapper
-        expect(AppWrapper.find(EventList)).toHaveLength(1); // expect() runs a search with find(), searching for EventList components within AppWrapper > only 1 should exist
+        const AppWrapper = shallow(<App />);
+        expect(AppWrapper.find(EventList)).toHaveLength(1);
     });
 });
