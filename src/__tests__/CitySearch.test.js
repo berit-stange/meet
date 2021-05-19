835fb5e0c4bf3430b739f89eaa30981654099426
berit-stange <76659950+berit-stange@users.noreply.github.com>
2021-05-19 13:43:11 +0200

    4.3 scen2/te4 - wrote test: do functions work?
    - define a new constant "query"
    - set to the query element from the CitySearch state
    - compare value prop of each element with class "city" within the CitySearch
    - check if input field's value prop is equal to CitySearch query state

--- src/__tests__/CitySearch.test.js
+++ src/__tests__/CitySearch.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import CitySearch from '../CitySearch';
 
+
 describe('<CitySearch /> component', () => {
 
     test('render text input field', () => {
@@ -12,6 +13,12 @@ describe('<CitySearch /> component', () => {
     test('render list of suggestions', () => {
         const CitySearchWrapper = shallow(<CitySearch />);
         expect(CitySearchWrapper.find('.suggestions')).toHaveLength(1);
+    });
+
+    test('render text input correctly', () => {
+        const CitySearchWrapper = shallow(<CitySearch />);
+        const query = CitySearchWrapper.state('query');
+        expect(CitySearchWrapper.find('.city').prop('value')).toBe(query);
     });
 
 });
