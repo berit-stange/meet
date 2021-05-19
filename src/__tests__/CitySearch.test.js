fb4274bf229cfd4eca09836895364ed7b14102ad
berit-stange <76659950+berit-stange@users.noreply.github.com>
2021-05-19 16:09:52 +0200

    4.3 scen2 - refactored code with beforeAll() in CitySearch

--- src/__tests__/CitySearch.test.js
+++ src/__tests__/CitySearch.test.js
@@ -7,24 +7,25 @@ import { extractLocations } from '../api';
 
 describe('<CitySearch /> component', () => {
 
+    let CitySearchWrapper;
+    beforeAll(() => {
+        CitySearchWrapper = shallow(<CitySearch />);
+    });
+
     test('render text input field', () => {
-        const CitySearchWrapper = shallow(<CitySearch />);
         expect(CitySearchWrapper.find('.city')).toHaveLength(1);
     });
 
     test('render list of suggestions', () => {
-        const CitySearchWrapper = shallow(<CitySearch />);
         expect(CitySearchWrapper.find('.suggestions')).toHaveLength(1);
     });
 
     test('render text input correctly', () => {
-        const CitySearchWrapper = shallow(<CitySearch />);
         const query = CitySearchWrapper.state('query');
         expect(CitySearchWrapper.find('.city').prop('value')).toBe(query);
     });
 
     test('change state when text input changes', () => {
-        const CitySearchWrapper = shallow(<CitySearch />);
         CitySearchWrapper.setState({
             query: 'Berlin'
         });
@@ -35,7 +36,6 @@ describe('<CitySearch /> component', () => {
 
     test('render list of suggestions correctly', () => {
         const locations = extractLocations(mockData);
-        const CitySearchWrapper = shallow(<CitySearch />);
         CitySearchWrapper.setState({ suggestions: locations });
         const suggestions = CitySearchWrapper.state('suggestions');
         expect(CitySearchWrapper.find('.suggestions li')).toHaveLength(suggestions.length + 1);
