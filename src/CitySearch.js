da283c3b68df1f9b4e85e173cc7e31064d491663
berit-stange <76659950+berit-stange@users.noreply.github.com>
2021-05-19 14:33:29 +0200

    4.3 scen2/te5 - defined function to be used as event handler for input
    + created event listener onChange (added as a prop in React)
    + pass function as event handler ( this.handleInputChanged() )

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -4,6 +4,12 @@ class CitySearch extends Component {
     state = {
         query: '',
     }
+
+    handleInputChanged = (event) => {
+        const value = event.target.value;
+        this.setState({ query: value });
+    }
+
     render() {
         return (
             <div className="CitySearch">
@@ -11,6 +17,7 @@ class CitySearch extends Component {
                     type="text"
                     className="city"
                     value={this.state.query}
+                    onChange={this.handleInputChanged}
                 />
                 <ul className="suggestions">
                 </ul>
