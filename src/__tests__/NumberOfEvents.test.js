310435a27ac5bccbc6f9e87d0e802ae4aa7f7e34
berit-stange <76659950+berit-stange@users.noreply.github.com>
2021-05-20 13:04:39 +0200

    4.3 - refactored code for test suite in NumberOfEvents.test.js with beforeAll()

--- src/__tests__/NumberOfEvents.test.js
+++ src/__tests__/NumberOfEvents.test.js
@@ -4,25 +4,25 @@ import NumberOfEvents from '../NumberOfEvents';
 
 describe('<NumberOfEvents/> component', () => {
 
+    let NumberOfEventsWrapper;
+    beforeAll(() => {
+        NumberOfEventsWrapper = shallow(<NumberOfEvents />);
+    });
+
     test('does number container render?', () => {
-        const NumberOfEventsWrapper = shallow(<NumberOfEvents />);
         expect(NumberOfEventsWrapper.find('.select-number')).toHaveLength(1);
     });
 
     test('does container render?', () => {
-        const NumberOfEventsWrapper = shallow(<NumberOfEvents />);
         const number = NumberOfEventsWrapper.state('numberEvents');
         expect(NumberOfEventsWrapper.find('.number-input').prop('value')).toBe(number);
     });
 
     test('does state change on input change?', () => {
-        const NumberOfEventsWrapper = shallow(<NumberOfEvents />);
-
         NumberOfEventsWrapper.setState({
             numberEvents: 10
         });
         const eventObject = { target: { value: 30 } };
-
         NumberOfEventsWrapper.find('.number-input').simulate('change', eventObject);
         expect(NumberOfEventsWrapper.state('numberEvents')).toBe(30);
     });
