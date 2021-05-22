342cd23f16d21e7527467590efd117e8ec4a4019
berit-stange <76659950+berit-stange@users.noreply.github.com>
2021-05-22 15:19:01 +0200

    Integrating the Final API: added function to get token
    The code above starts by checking whether an access token was found. If no token is found (!accessToken), it then checks for an authorization code. If no authorization code is found (!code), the user is automatically redirected to the Google Authorization screen, where they can sign in and receive their code

--- src/api.js
+++ src/api.js
@@ -6,6 +6,7 @@
  * It will also remove all duplicates by creating another new array using the spread operator and spreading a Set.
  * The Set will remove all duplicates from the array.
  */
+import axios from 'axios';
 import { mockData } from './mock-data';
 
 export const extractLocations = (events) => {
@@ -17,3 +18,23 @@ export const extractLocations = (events) => {
 export const getEvents = async () => {
     return mockData;
 };
+
+export const getAccessToken = async () => {
+    const accessToken = localStorage.getItem('access_token');
+
+    const tokenCheck = accessToken && (await checkToken(accessToken));
+
+    if (!accessToken || tokenCheck.error) {
+        await localStorage.removeItem('access_token');
+        const searchParams = new URLSearchParams(window.location.search);
+        const code = await searchParams.get('code');
+        if (!code) {
+            const results = await axios.get(
+                'https://szno1iumgi.execute-api.eu-central-1.amazonaws.com/dev/api/get-auth-url'
+            );
+            const { authUrl } = results.data;
+            return (window.location.href = authUrl);
+        }
+        return accessToken;
+    }
+}
