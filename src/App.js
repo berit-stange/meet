2f776f04cd1bbb91694d687cd42416e448286d51
berit-stange <76659950+berit-stange@users.noreply.github.com>
2021-06-02 13:03:12 +0200

    removed name in tooltip at scatter chart

--- src/App.js
+++ src/App.js
@@ -114,21 +114,21 @@ class App extends Component {
           />
         </div>
 
-        {/* <div className="data-vis-wrapper" > */}
-        <EventGenre events={events} />
+        <div className="data-vis-wrapper" >
+          <EventGenre events={events} />
 
-        <ResponsiveContainer height={300} /* width="50%" height="50%" */ >
+          <ResponsiveContainer height={300} /* width="50%" height="50%" */ >
 
-          <ScatterChart
-            margin={{ top: 20, right: 20, bottom: 10, left: 10 }}>
-            <CartesianGrid strokeDasharray="3 3" />
-            <XAxis type="category" dataKey="city" name="city" />
-            <YAxis type="number" dataKey="number" name="number of events" />
-            <Tooltip cursor={{ strokeDasharray: '3 3' }} />
-            <Scatter name="A school" data={this.getData()} fill="#e25d3c" />
-          </ScatterChart>
-        </ResponsiveContainer>
-        {/* </div> */}
+            <ScatterChart
+              margin={{ top: 20, right: 20, bottom: 10, left: 10 }}>
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis type="category" dataKey="city" name="city" />
+              <YAxis type="number" dataKey="number" name="number of events" />
+              <Tooltip cursor={{ strokeDasharray: '3 3' }} />
+              <Scatter data={this.getData()} fill="#e25d3c" />
+            </ScatterChart>
+          </ResponsiveContainer>
+        </div>
 
         <EventList
           events={this.state.events}
